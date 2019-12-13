7cb40625ac3d5b4bebe65121e4a64fdd865bda0d
Brett Longworth <blongworth@gmail.com>
2019-12-13 14:07:33 -0500

    First commit

---
+++ R/hgis_data_reduction_functions.R
@@ -0,0 +1,18 @@
+# Functions for HGIS data reduction
+
+
+# Load data
+
+# Trim bad blocks
+
+# Add 13C and deadtime correction
+
+
+## Normalize
+# Find standards
+# Normalize to mean of standards
+# Propagate error
+
+## Blank correction
+# Apply large blank
+# Propagate large blank error
