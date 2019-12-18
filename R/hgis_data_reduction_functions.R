f4e87757084cd73209ae904c184b2ea7eb3be9e1
Brett Longworth <blongworth@gmail.com>
2019-12-18 17:23:58 -0500

    Add more functions

--- R/hgis_data_reduction_functions.R
+++ R/hgis_data_reduction_functions.R
@@ -4,12 +4,11 @@
 # Load data
 
 # Calculate per-block fields
-
 calcRaw <- function(data) {
 	data %>%
 	  mutate(
-            ltcorr = CntTotS - CntTotH,
-	    cor1412 = doCorr(he1412, ltcorr, he1312),
+      ltcorr = CntTotS - CntTotH,
+	    cor1412 = doCor1412(he1412, ltcorr, he1312),
 	    sig1412 = ,
 	    d13c = calcd13c(he1312)
 	  )
@@ -18,8 +17,15 @@ calcRaw <- function(data) {
 # Trim bad blocks
 
 # Add 13C and deadtime correction
-doCorr <- function(he1412, ltcorr, he1312) {
+doCor1412 <- function(he1412, ltcorr, he1312) {
 	he1412 / ltcorr / he1312 ^ 2
+}
+
+# Calc internal error for a measurement
+calcSig1412 <- function(CntTotH, CntTotS, CntTotGT, cor1412) {
+RelErrSq <- (CntTotH - CntTotS) * CntTotH ^ 2 / CntTotS ^ 4 + 
+             CntTotH ^ 2 / CntTotGT / CntTotS ^ 2
+cor1412 * RelErrSq ^ 0.5
 }
 
 # Calculate d13C
@@ -29,9 +35,20 @@ calcd13c <- function(he1312) {
 
 ## Normalize
 # Find standards
-# Normalize to mean of standards
 
-# Propagate error
+
+# Find mean of stds
+normStds <- function(cor1412std, defstd) {
+  # both vectors, same length or 1 element vector if all standards are same
+  mean(cor1412std/defstd) 
+}
+
+# Normalize to mean of standards
+norm1412 <- function(cor1412, meanstd) {
+  cor1412/meanstd
+}
+
+# Propagate normalization error
 
 ## Blank correction
 # Apply large blank
@@ -45,3 +62,5 @@ doLBCerror <- function(fmmeas, fmblank, fmstd, fmmeaserr, fmblankerr) {
 	fmmeaserr ^ 2 * (1 + fmblank / fmstd) ^ 2 +
 	fmblankerr ^ 2 * ((fmmeas - fmstd) / fmstd) ^ 2
 }
+
+# Produce normalized data for wheel per target
